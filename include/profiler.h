db64dbc39eefd5296c07ddba5be84fcf7e8a0363
BitDragon256 <bergben44@gmail.com>
2023-11-13 23:39:32 +0100

    buffered profiler output

--- include/profiler.h
+++ include/profiler.h
@@ -2,9 +2,11 @@
 
 #include <chrono>
 #include <string>
+#include <sstream>
 #include <unordered_map>
 
 #define NANOSECONDS_PER_SECOND 1000000000.f
+#define PROFILER_OUT_BUFFER_WRITES 500 
 
 class Profiler
 {
@@ -15,14 +17,22 @@ public:
 
 	float passing_measure(std::string name);
 	void print_last_measure(std::string name);
+	void begin_label(std::string name);
+	float end_label();
+
+	std::stringstream& out_buf();
 
 private:
 
 	std::unordered_map<std::string, std::chrono::time_point<std::chrono::high_resolution_clock>> m_measures;
 	std::unordered_map<std::string, float> m_lastMeasures;
+	std::vector<std::string> m_labels;
 
 	void save_time(std::string name);
 	std::chrono::time_point<std::chrono::high_resolution_clock> now();
 	float measure(std::string name);
+
+	std::stringstream m_outS;
+	size_t m_outBufWrites = 0;
 
 };
--- src/profiler.cpp
+++ src/profiler.cpp
@@ -10,10 +10,13 @@ void Profiler::start_measure(std::string name)
 }
 float Profiler::end_measure(std::string name, bool print)
 {
+    //auto renderStart = std::chrono::high_resolution_clock::now();
 	float time = measure(name);
 	m_measures.erase(name);
 	if (print)
 		print_last_measure(name);
+    //auto renderEnd = std::chrono::high_resolution_clock::now();
+    //out_buf() << "measure time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(renderEnd - renderStart).count() / NANOSECONDS_PER_SECOND;
 	return time;
 }
 float Profiler::passing_measure(std::string name)
@@ -26,7 +29,30 @@ float Profiler::passing_measure(std::string name)
 }
 void Profiler::print_last_measure(std::string name)
 {
-	std::cout << name << " measured " << m_lastMeasures[name] << " seconds\n";
+	std::string indent = "";
+	for (int i = 0; i < m_labels.size(); i++)
+		indent += "-";
+	if (!m_labels.empty())
+		indent += " ";
+	out_buf() << indent << name << " measured " << m_lastMeasures[name] << " seconds\n";
+}
+void Profiler::begin_label(std::string name)
+{
+	out_buf() << "----------" << name << "----------\n";
+	m_labels.push_back(name);
+	start_measure(name);
+}
+float Profiler::end_label()
+{
+	if (!m_labels.empty())
+	{
+		std::string name = m_labels.back();
+		m_outS << "---";
+		float time = end_measure(name, true);
+		m_labels.pop_back();
+		return time;
+	}
+	return 0.f;
 }
 
 void Profiler::save_time(std::string name)
@@ -42,4 +68,15 @@ float Profiler::measure(std::string name)
 std::chrono::time_point<std::chrono::high_resolution_clock> Profiler::now()
 {
 	return std::chrono::high_resolution_clock::now();
+}
+std::stringstream& Profiler::out_buf()
+{
+	if (m_outBufWrites++ >= PROFILER_OUT_BUFFER_WRITES)
+	{
+		std::ios::sync_with_stdio(false);
+		std::cout << m_outS.str();
+		m_outS.clear();
+		m_outBufWrites = 0;
+	}
+	return m_outS;
 }
