3596d6dbd5658b348329b01f16574437e3de7fcc
BitDragon256 <bergben44@gmail.com>
2023-11-17 23:58:14 +0100

    profiling buffer and seperate copy submission

--- include/buffer.h
+++ include/buffer.h
@@ -10,6 +10,20 @@
 #include "nve_types.h"
 #include "vulkan_helpers.h"
 #include "logger.h"
+#include "profiler.h"
+
+#define BUFFER_PROFILER
+#ifdef BUFFER_PROFILER 
+#define PROFILE_START(X) m_profiler.start_measure(X);
+#define PROFILE_END(X) m_profiler.end_measure(X, true);
+#define PROFILE_LABEL(X) m_profiler.begin_label(X);
+#define PROFILE_LABEL_END m_profiler.end_label();
+#else
+#define PROFILE_START(X)
+#define PROFILE_END(X) 0.f;
+#define PROFILE_LABEL(X)
+#define PROFILE_LABEL_END
+#endif
 
 typedef struct {
     VkDevice device;
@@ -22,6 +36,7 @@ typedef struct {
     bool singleUseStagedBuffer;
     VkCommandPool stagedBufferTransferCommandPool;
     VkQueue stagedBufferTransferQueue;
+    bool selfSubmitStagedBufferCopy;
 } BufferConfig;
 
 typedef struct {
@@ -55,6 +70,19 @@ inline void destroy_all_corresponding_buffers(VkDevice device)
     }
 }
 
+inline bool memequal(void* a, void* b, size_t stride)
+{
+    if (stride % 4 != 0)
+    {
+        std::cerr << "stride must be multiple of four!\n";
+        abort();
+    }
+    for (size_t i = 0; i < stride; i += 4)
+        if (*((int*)a + i) != *((int*)b + i))
+            return false;
+    return true;
+}
+
 template<class T>
 class RawBuffer
 {
@@ -62,18 +90,37 @@ public:
     RawBuffer() :
         m_created{ false }, m_initialized{ false }, m_data(), m_realSize{ 0 } {}
 
-    void set(const std::vector<T>& data)
+    // sets the data of the buffer and returns false when the data is not new
+    bool set(const std::vector<T>& data)
     {
         if (!m_initialized || data.size() == 0)
-            return;
+            return false;
 
         bool recreate = data.size() > m_data.size() || !m_created;
         m_data = data;
-        m_realSize = sizeof(T) * m_data.size();
+        size_t tSize = sizeof(T);
+        m_realSize = tSize * m_data.size();
 
+        std::string typeName = typeid(*this).name();
+        PROFILE_START("recreate buffer " + typeName);
         if (recreate)
             create();
-        reload_data();
+        PROFILE_END("recreate buffer" + typeName);
+        PROFILE_START("check reload");
+        bool reload = recreate;
+        for (size_t i = 0; i < data.size() && !reload; i++)
+            if (memequal((void*) & m_data[i], (void*) & data[i], tSize))
+                reload = true;
+        PROFILE_END("check reload");
+        if (reload)
+        {
+            PROFILE_LABEL("reload data " + typeName);
+            reload_data();
+            PROFILE_LABEL_END
+        }
+        else
+            return false;
+        return true;
     }
     void cpy_raw(uint32_t size, const T* data)
     {
@@ -125,9 +172,15 @@ protected:
     void cpy_data(const T* d)
     {
         void* data;
+        PROFILE_START("map mem");
         vkMapMemory(m_config.device, m_memory, 0, m_realSize, 0, &data);
+        PROFILE_END("map mem");
+        PROFILE_START("cpy mem");
         std::memcpy(data, d, (size_t)m_realSize);
+        PROFILE_END("cpy mem");
+        PROFILE_START("unmap mem");
         vkUnmapMemory(m_config.device, m_memory);
+        PROFILE_END("unmap mem");
     }
 
     bool m_created;
@@ -173,6 +226,8 @@ protected:
         vkBindBufferMemory(m_config.device, buffer, memory, 0);
     }
 
+    Profiler m_profiler;
+
 private:
 
     BufferDestructionInfo destruction_info()
@@ -211,6 +266,17 @@ public:
         {
             m_config.usage |= VK_BUFFER_USAGE_TRANSFER_DST_BIT;
             m_config.memoryFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+
+            VkCommandBufferAllocateInfo cmdBufAI;
+            cmdBufAI.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+            cmdBufAI.pNext = 0;
+            cmdBufAI.commandBufferCount = 1;
+            cmdBufAI.commandPool = m_config.stagedBufferTransferCommandPool;
+            if (m_config.selfSubmitStagedBufferCopy)
+                cmdBufAI.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+            else
+                cmdBufAI.level = VK_COMMAND_BUFFER_LEVEL_SECONDARY;
+            vkAllocateCommandBuffers(m_config.device, &cmdBufAI, &m_commandBuffer);
         }
 
         m_stagingBuffer.initialize(m_config.device, m_config.physicalDevice);
@@ -228,6 +294,10 @@ public:
     {
         m_stagingBuffer.destroy();
         RawBuffer<T>::destroy();
+    }
+    VkCommandBuffer staged_cpy_cmd_buf()
+    {
+        return m_commandBuffer;
     }
 protected:
     void reload_data() override
@@ -250,12 +320,58 @@ protected:
 
     void copy_buffer(VkBuffer srcBuf, VkBuffer dstBuf, VkDeviceSize size)
     {
-        VkCommandBuffer cmdBuffer = begin_single_time_cmd_buffer(m_config.stagedBufferTransferCommandPool, m_config.device);
+        PROFILE_START("begin cmd buf");
+        vkResetCommandBuffer(m_commandBuffer, 0);
+        VkCommandBufferBeginInfo cmdBufBI;
+        cmdBufBI.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        cmdBufBI.flags = 0;
+        cmdBufBI.pNext = 0;
+        cmdBufBI.pInheritanceInfo = 0;
+        vkBeginCommandBuffer(m_commandBuffer, &cmdBufBI);
+        // VkCommandBuffer cmdBuffer = begin_single_time_cmd_buffer(m_config.stagedBufferTransferCommandPool, m_config.device);
+        PROFILE_END("begin cmd buf");
         VkBufferCopy copyRegion = {};
         copyRegion.size = RawBuffer<T>::m_realSize;
         copyRegion.srcOffset = 0;
         copyRegion.dstOffset = 0;
-        vkCmdCopyBuffer(cmdBuffer, srcBuf, dstBuf, 1, &copyRegion);
-        end_single_time_cmd_buffer(cmdBuffer, m_config.stagedBufferTransferCommandPool, m_config.device, m_config.stagedBufferTransferQueue);
+        PROFILE_START("record cmd buf");
+        vkCmdCopyBuffer(m_commandBuffer, srcBuf, dstBuf, 1, &copyRegion);
+        PROFILE_END("record cmd buf");
+        PROFILE_START("submit cmd buf");
+        // end_single_time_cmd_buffer(cmdBuffer, m_config.stagedBufferTransferCommandPool, m_config.device, m_config.stagedBufferTransferQueue);
+        vkEndCommandBuffer(m_commandBuffer);
+
+        if (m_config.selfSubmitStagedBufferCopy)
+        {
+            VkSubmitInfo submitInfo{};
+            submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+            submitInfo.commandBufferCount = 1;
+            submitInfo.pCommandBuffers = &m_commandBuffer;
+
+            VkFence fence;
+            VkFenceCreateInfo fenceCI = {};
+            fenceCI.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+            fenceCI.pNext = nullptr;
+            fenceCI.flags = 0;
+
+            vkCreateFence(m_config.device, &fenceCI, nullptr, &fence);
+
+            res = vkQueueSubmit(m_config.stagedBufferTransferQueue, 1, &submitInfo, fence);
+            res = vkWaitForFences(m_config.device, 1, &fence, VK_TRUE, UINT64_MAX);
+
+            vkDestroyFence(m_config.device, fence, nullptr);
+        }
+
+        PROFILE_END("submit cmd buf");
     }
+
+private:
+
+    Profiler m_profiler;
+    VkCommandBuffer m_commandBuffer;
 };
+
+#undef PROFILE_START(X)
+#undef PROFILE_END(X)
+#undef PROFILE_LABEL(X)
+#undef PROFILE_LABEL_END
