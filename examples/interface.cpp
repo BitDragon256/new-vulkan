8e8b9908fedeed5cfe2eeb1d3d22400004086c1a
Benedict Sondershaus <bergben44@gmail.com>
2024-03-17 19:43:07 +0100

    better pipeline switch

--- examples/interface.cpp
+++ examples/interface.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#include <chrono>
+#include <functional>
+
 #include <nve.h>
 
 int main(int argc, char** argv)
@@ -12,18 +15,26 @@ int main(int argc, char** argv)
       renderer.m_ecs.get_component<DynamicModel>(cube).set_fragment_shader("fragments/lamb_wmat.frag.spv");
       renderer.active_camera().m_position = Vector3{ -2.f, 0.f, 1.f };
       renderer.active_camera().m_rotation = Vector3{ 0.f, 20.f, 0.f };
-      for (int i = 0; i < 5000; i++)
+      auto time = std::chrono::high_resolution_clock::now();
+      auto delta = [&]() {
+            auto cur = std::chrono::high_resolution_clock::now();
+            auto last = time;
+            time = cur;
+            return std::chrono::duration_cast<std::chrono::microseconds>(cur - last).count() / 1000000.f;
+      };
+      float total = 0.f;
+      while (true)
       {
             auto res = renderer.render();
-            if (res == NVE_RENDER_EXIT_SUCCESS)
+            if (res == NVE_RENDER_EXIT_SUCCESS || total >= 10.f)
                   break;
-            std::cout << "\r" << i;
-            if (i % 400 == 0)
+            total += delta();
+            if (((int) total) % 2 == 0)
             {
                   renderer.m_ecs.get_component<DynamicModel>(cube).set_fragment_shader("fragments/lamb_wmat.frag.spv");
                   renderer.reload_pipelines();
             }
-            else if (i % 400 == 200)
+            else if (((int) total) % 2 == 1)
             {
                   renderer.m_ecs.get_component<DynamicModel>(cube).set_fragment_shader("fragments/unlit_wmat.frag.spv");
                   renderer.reload_pipelines();
