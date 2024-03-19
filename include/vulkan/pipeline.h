e425c86746b922322ea92cc1485e9ca777499bc8
Benedict Sondershaus <bergben44@gmail.com>
2024-03-19 12:25:35 +0100

    pipeline abstraction

--- include/vulkan/pipeline.h
+++ include/vulkan/pipeline.h
@@ -1,8 +1,122 @@
 #pragma once
 
+#include <array>
+#include <memory>
+#include <vector>
+
+#include <vulkan/vulkan.h>
+
+#include "nve_types.h"
+#include "material.h"
+
+using RenderPassRef = std::shared_ptr<VkRenderPass>;
+
+class PipelineLayout
+{
+public:
+
+      void create(std::vector<VkDescriptorSetLayout> descriptorSetLayouts, std::vector<VkPushConstantRange> pushConstants, VkDevice device);
+
+      VkPipelineLayout m_layout;
+
+private:
+};
+
+typedef std::shared_ptr<PipelineLayout> PipelineLayoutRef;
+
+enum PipelineType { GraphicsPipeline_E = 0, RayTracingPipeline_E = 1, ComputePipeline_E = 2 };
+
 class Pipeline
 {
 public:
 
+	Pipeline(PipelineType type);
+	void initialize(VkDevice device, PipelineLayoutRef layout);
+	virtual void create_create_info() = 0;
+	virtual void create() = 0;
+
+	const PipelineType m_type;
+
+	VkDevice m_device;
+
+      VkPipeline m_pipeline;
+      PipelineLayoutRef m_layout;
+
+protected:
+
+};
+
+typedef std::shared_ptr<Pipeline> PipelineRef;
+
+typedef struct GraphicsPipelineCreationData_T
+{
+	std::vector<VkPipelineShaderStageCreateInfo>	stages;
+
+	VkPipelineVertexInputStateCreateInfo	      vertexInputState;
+	std::array<
+		VkVertexInputAttributeDescription,
+		VERTEX_ATTRIBUTE_COUNT
+	>						      	vertexAttributeDescriptions;
+	VkVertexInputBindingDescription	      	vertexBindingDescription;
+
+	VkPipelineInputAssemblyStateCreateInfo		inputAssemblyState;
+	VkPipelineTessellationStateCreateInfo		tessellationState;
+	VkPipelineViewportStateCreateInfo			viewportState;
+	VkPipelineRasterizationStateCreateInfo		rasterizationState;
+	VkPipelineMultisampleStateCreateInfo		multisampleState;
+	VkPipelineDepthStencilStateCreateInfo		depthStencilState;
+
+	VkPipelineColorBlendStateCreateInfo			colorBlendState;
+	VkPipelineColorBlendAttachmentState			colorBlendAttachment;
+
+	VkPipelineDynamicStateCreateInfo			dynamicState;
+	std::vector<VkDynamicState>				dynamicStates;
+} GraphicsPipelineCreationData;
+
+class GraphicsPipeline : public Pipeline
+{
+public:
+
+	GraphicsPipeline();
+	void create_create_info() override;
+	void create() override;
+
+	void initialize(RenderPassRef renderPass, uint32_t subpass);
+
+	VkGraphicsPipelineCreateInfo m_createInfo;
+
 private:
+
+	GraphicsPipelineCreationData m_creationData;
+	void set_default_create_info();
+	void set_shader_stages();
+
+	uint32_t m_subpass;
+	RenderPassRef m_renderPass;
+	GraphicsShader m_shader;
+
+};
+
+class PipelineBatchCreator
+{
+public:
+
+	PipelineBatchCreator();
+
+	void schedule_creation(PipelineRef pipeline);
+	void create_all();
+
+private:
+
+	void create_cache();
+	void create_graphics_pipelines();
+	void create_raytracing_pipelines();
+	void create_compute_pipelines();
+
+	std::vector<PipelineRef> m_pipelines;
+
+	VkPipelineCache m_cache;
+	VkDevice m_device;
+	bool m_cacheCreated;
+
 };
--- src/vulkan/pipeline.cpp
+++ src/vulkan/pipeline.cpp
@@ -1,2 +1,378 @@
 #include "vulkan/pipeline.h"
 
+#include <functional>
+#include <unordered_map>
+
+// -------------------------------------------
+// PIPELINE LAYOUT
+// -------------------------------------------
+
+void PipelineLayout::create(
+      std::vector<VkDescriptorSetLayout> descriptorSetLayouts,
+      std::vector<VkPushConstantRange> pushConstants,
+      VkDevice device
+)
+{
+      VkPipelineLayoutCreateInfo pipelineLayoutCI = {};
+      pipelineLayoutCI.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+      pipelineLayoutCI.pNext = nullptr;
+      pipelineLayoutCI.flags = 0;
+
+      pipelineLayoutCI.pSetLayouts = descriptorSetLayouts.data();
+      pipelineLayoutCI.setLayoutCount = static_cast<uint32_t>(descriptorSetLayouts.size());
+
+      pipelineLayoutCI.pPushConstantRanges = pushConstants.data();
+      pipelineLayoutCI.pushConstantRangeCount = static_cast<uint32_t>(pushConstants.size());
+
+      auto res = vkCreatePipelineLayout(device, &pipelineLayoutCI, nullptr, &m_layout);
+}
+
+// --------------------------------------
+// PIPELINE
+// --------------------------------------
+
+Pipeline::Pipeline(PipelineType type) :
+      m_type{ type },
+      m_pipeline{ VK_NULL_HANDLE }, m_layout{ VK_NULL_HANDLE }
+{
+      
+}
+void Pipeline::initialize(VkDevice device, PipelineLayoutRef layout)
+{
+	m_device = device;
+	m_layout = layout;
+}
+
+// --------------------------------------
+// GRAPHICS PIPELINE
+// --------------------------------------
+
+GraphicsPipeline::GraphicsPipeline() :
+      Pipeline::Pipeline(GraphicsPipeline_E)
+{
+
+}
+void GraphicsPipeline::initialize(RenderPassRef renderPass, uint32_t subpass)
+{
+	m_renderPass = renderPass;
+	m_subpass = subpass;
+}
+void GraphicsPipeline::create_create_info()
+{
+	set_default_create_info();
+	set_shader_stages();
+
+	m_createInfo.layout = m_layout->m_layout;
+	m_createInfo.renderPass = *m_renderPass;
+	m_createInfo.subpass = m_subpass;
+}
+void GraphicsPipeline::create()
+{
+      vkCreateGraphicsPipelines(
+            m_device, VK_NULL_HANDLE,
+            1,
+            &m_createInfo,
+            nullptr,
+            &m_pipeline
+      );
+}
+
+void GraphicsPipeline::set_default_create_info()
+{
+	m_createInfo = {};
+	m_createInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+	m_createInfo.pNext = nullptr;
+	m_createInfo.flags = 0;
+
+	// ---------------------------------------
+
+	m_creationData.vertexInputState = {};
+	m_creationData.vertexInputState.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+	m_creationData.vertexInputState.pNext = nullptr;
+	m_creationData.vertexInputState.flags = 0;
+
+	m_creationData.vertexAttributeDescriptions = Vertex::getAttributeDescriptions();
+	m_creationData.vertexInputState.vertexAttributeDescriptionCount = 0;
+	m_creationData.vertexInputState.vertexAttributeDescriptionCount = m_creationData.vertexAttributeDescriptions.size();
+	m_creationData.vertexInputState.pVertexAttributeDescriptions = m_creationData.vertexAttributeDescriptions.data();
+
+	m_creationData.vertexBindingDescription = Vertex::getBindingDescription();
+	m_creationData.vertexInputState.vertexBindingDescriptionCount = 0;
+	m_creationData.vertexInputState.vertexBindingDescriptionCount = 1;
+	m_creationData.vertexInputState.pVertexBindingDescriptions = &m_creationData.vertexBindingDescription;
+
+	m_createInfo.pVertexInputState = &m_creationData.vertexInputState;
+
+	// -------------------------------------------
+
+	m_creationData.inputAssemblyState = {};
+	m_creationData.inputAssemblyState.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+	m_creationData.inputAssemblyState.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+	m_creationData.inputAssemblyState.primitiveRestartEnable = VK_FALSE;
+
+	m_createInfo.pInputAssemblyState = &m_creationData.inputAssemblyState;
+
+	// -------------------------------------------
+
+	m_creationData.viewportState = {};
+	m_creationData.viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+	m_creationData.viewportState.viewportCount = 1;
+	m_creationData.viewportState.scissorCount = 1;
+
+	m_createInfo.pViewportState = &m_creationData.viewportState;
+
+	// -------------------------------------------
+
+	m_creationData.rasterizationState = {};
+	m_creationData.rasterizationState.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+	m_creationData.rasterizationState.depthClampEnable = VK_FALSE;
+	m_creationData.rasterizationState.rasterizerDiscardEnable = VK_FALSE;
+	m_creationData.rasterizationState.polygonMode = VK_POLYGON_MODE_FILL;
+	m_creationData.rasterizationState.lineWidth = 1.0f;
+	m_creationData.rasterizationState.cullMode = VK_CULL_MODE_BACK_BIT;
+	m_creationData.rasterizationState.frontFace = VK_FRONT_FACE_CLOCKWISE;
+	m_creationData.rasterizationState.depthBiasEnable = VK_FALSE;
+
+	m_createInfo.pRasterizationState = &m_creationData.rasterizationState;
+
+	// -------------------------------------------
+
+	m_creationData.multisampleState = {};
+	m_creationData.multisampleState.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+	m_creationData.multisampleState.sampleShadingEnable = VK_FALSE;
+	m_creationData.multisampleState.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+	m_createInfo.pMultisampleState = &m_creationData.multisampleState;
+
+	// -------------------------------------------
+	
+	m_creationData.depthStencilState = {};
+	m_creationData.depthStencilState.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+	m_creationData.depthStencilState.pNext = nullptr;
+	m_creationData.depthStencilState.flags = 0;
+	m_creationData.depthStencilState.depthTestEnable = VK_TRUE;
+	m_creationData.depthStencilState.depthWriteEnable = VK_TRUE;
+	m_creationData.depthStencilState.depthCompareOp = VK_COMPARE_OP_LESS;
+	m_creationData.depthStencilState.depthBoundsTestEnable = VK_FALSE;
+	m_creationData.depthStencilState.stencilTestEnable = VK_FALSE;
+
+	m_createInfo.pDepthStencilState = &m_creationData.depthStencilState;
+
+	// -------------------------------------------
+
+	m_creationData.colorBlendAttachment = {};
+	m_creationData.colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+	m_creationData.colorBlendAttachment.blendEnable = VK_TRUE;
+	m_creationData.colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+	m_creationData.colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+	m_creationData.colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+	m_creationData.colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+	m_creationData.colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+	m_creationData.colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
+
+	m_creationData.colorBlendState = {};
+	m_creationData.colorBlendState.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+	m_creationData.colorBlendState.logicOpEnable = VK_FALSE;
+	m_creationData.colorBlendState.logicOp = VK_LOGIC_OP_COPY;
+	m_creationData.colorBlendState.attachmentCount = 1;
+	m_creationData.colorBlendState.pAttachments = &m_creationData.colorBlendAttachment;
+	m_creationData.colorBlendState.blendConstants[0] = 0.0f;
+	m_creationData.colorBlendState.blendConstants[1] = 0.0f;
+	m_creationData.colorBlendState.blendConstants[2] = 0.0f;
+	m_creationData.colorBlendState.blendConstants[3] = 0.0f;
+
+	m_createInfo.pColorBlendState = &m_creationData.colorBlendState;
+
+	// -------------------------------------------
+
+	m_creationData.dynamicStates = {
+	   VK_DYNAMIC_STATE_VIEWPORT,
+	   VK_DYNAMIC_STATE_SCISSOR
+	};
+	m_creationData.dynamicState = {};
+	m_creationData.dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+	m_creationData.dynamicState.dynamicStateCount = static_cast<uint32_t>(m_creationData.dynamicStates.size());
+	m_creationData.dynamicState.pDynamicStates = m_creationData.dynamicStates.data();
+
+	m_createInfo.pDynamicState = &m_creationData.dynamicState;
+
+	// -------------------------------------------
+
+	m_createInfo.basePipelineHandle = VK_NULL_HANDLE;
+	m_createInfo.basePipelineIndex = -1;
+
+	// -------------------------------------------
+}
+void GraphicsPipeline::set_shader_stages()
+{
+	VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+	vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+	vertShaderStageInfo.module = m_shader->vertex.m_module;
+	vertShaderStageInfo.pName = "main";
+
+	VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+	fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+	fragShaderStageInfo.module = m_shader->fragment.m_module;
+	fragShaderStageInfo.pName = "main";
+
+	m_creationData.stages.push_back(vertShaderStageInfo);
+	m_creationData.stages.push_back(fragShaderStageInfo);
+
+	m_createInfo.stageCount = static_cast<uint32_t>(m_creationData.stages.size());
+	m_createInfo.pStages = m_creationData.stages.data();
+}
+
+// --------------------------------------
+// PIPELINE BATCH CREATOR
+// --------------------------------------
+
+PipelineBatchCreator::PipelineBatchCreator() :
+	m_cacheCreated{ false }
+{}
+
+void PipelineBatchCreator::schedule_creation(
+      PipelineRef pipeline
+)
+{
+      m_pipelines.push_back(pipeline);
+}
+void PipelineBatchCreator::create_all()
+{
+	create_cache();
+
+	create_graphics_pipelines();
+	create_compute_pipelines();
+	create_raytracing_pipelines();
+
+	m_pipelines.clear();
+}
+
+// --------------------------------------
+// PRIVATE
+// --------------------------------------
+
+void PipelineBatchCreator::create_cache()
+{
+	if (m_cacheCreated || m_pipelines.empty())
+		return;
+
+	m_device = m_pipelines.front()->m_device;
+
+	VkPipelineCacheCreateInfo cacheCI = {};
+	cacheCI.sType = VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO;
+	cacheCI.pNext = nullptr;
+	cacheCI.flags = 0;
+
+	cacheCI.initialDataSize = 0;
+	cacheCI.pInitialData = nullptr;
+
+	vkCreatePipelineCache(m_device, &cacheCI, nullptr, &m_cache);
+
+	m_cacheCreated = true;
+}
+
+// stores all pipelines of the given type (e.g. graphics, compute, ...) into `pipelines`
+template<typename P> void get_specific_pipelines(const std::vector<PipelineRef>& allPipelines, std::vector<P*>& pipelines, PipelineType type)
+{
+	for (auto pipeline : allPipelines)
+	{
+		if (pipeline->m_type = type)
+		{
+			auto specificPipeline = dynamic_cast<P*>(pipeline->get());
+			pipelines.push_back(specificPipeline);
+		}
+	}
+}
+
+// sorts the given pipelines into buckets without destroying the already given buckets
+// returns the start of the newly sorted data (the old data is in front of it)
+template<typename T> size_t sort_into_buckets(std::vector<std::vector<GraphicsPipeline*>>& partitionedPipelines, size_t startIndex, std::function<T(GraphicsPipeline*)> accessor)
+{
+	std::unordered_map<T, std::vector<GraphicsPipeline*>> mappedPipelines;
+	size_t endIndex = partitionedPipelines.size();
+	for (size_t i = startIndex; i < endIndex; i++)
+	{
+		mappedPipelines.clear();
+		for (auto pipeline : partitionedPipelines[i])
+			mappedPipelines[accessor(pipeline)].push_back(pipeline);
+		for (auto [device, p] : deviceMappedPipelines)
+			partitionedPipelines.push_back(p);
+	}
+	return endIndex;
+}
+
+// creates the pipelines of a specific type (graphics, compute, ...) given with `T`
+// `C` is the corresponding VkCreateInfo type
+// `createPipelines` contains the vulkan function to actually create the pipelines (e.g. vkCreateGraphicsPipelines(...))
+template<typename T, typename C>
+void create_specific_pipelines(
+	std::vector<PipelineRef>& m_pipelines, VkPipelineCache& m_cache,
+	std::function<void(VkDevice, VkPipelineCache, const std::vector<C>&, std::vector<VkPipeline>&)> createPipelines
+)
+{
+	std::vector<std::vector<T*>> partitionedPipelines = { std::vector<T*>() };
+
+	get_specific_pipelines(m_pipelines, partitionedPipelines.front(), GraphicsPipeline_E);
+
+	size_t start = 0;
+
+	start = sort_into_buckets<PipelineLayoutRef>(partitionedPipelines, start, [](T* pipeline) { return pipeline->m_layout; });
+
+	partitionedPipelines.erase(
+		partitionedPipelines.begin(),
+		partitionedPipelines.begin() + start
+	);
+
+	for (const auto& pipelines : partitionedPipelines)
+	{
+		std::vector<VkPipeline> vulkanPipelines(pipelines.size());
+		std::vector<C> createInfos(pipelines.size());
+		for (size_t i = 0; i < pipelines.size(); i++)
+		{
+			vulkanPipelines[i] = pipelines[i]->m_pipeline;
+			createInfos[i] = pipelines[i]->m_createInfo;
+		}
+
+		createPipelines(
+			m_device,
+			m_cache,
+			createInfos,
+			vulkanPipelines
+		);
+
+		for (size_t i = 0; i < pipelines.size(); i++)
+			pipelines[i]->m_pipeline = vulkanPipelines[i];
+	}
+}
+
+void PipelineBatchCreator::create_graphics_pipelines()
+{
+	create_specific_pipelines<GraphicsPipeline, VkGraphicsPipelineCreateInfo>(
+		m_pipelines, m_cache,
+		[](
+			VkDevice device, VkPipelineCache cache,
+			const std::vector<VkGraphicsPipelineCreateInfo>& createInfos,
+			std::vector<VkPipeline>& pipelines
+			)
+		{
+			vkCreateGraphicsPipelines(
+				device,
+				cache,
+				createInfos.size(),
+				createInfos.data(),
+				nullptr,
+				pipelines.data()
+			);
+		}
+	);
+}
+void PipelineBatchCreator::create_compute_pipelines()
+{
+
+}
+void PipelineBatchCreator::create_raytracing_pipelines()
+{
+
+}
